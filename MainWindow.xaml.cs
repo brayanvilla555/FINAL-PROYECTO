0cd6662cd8741fa5e028aa1b32ae64ba65db09bb
Yato <Yato@YAMOTSUKI>
2024-03-02 18:08:57 -0500

    hola

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FINAL_PROYECTO
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
