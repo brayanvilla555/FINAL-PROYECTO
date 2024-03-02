81ce69a3599d71be5833c882c2288fc34c9bbb8b
brayanvilla555 <brayanalexander098@gmail.com>
2024-03-02 18:16:59 -0500

    Creación a la BD, clase conexion

---
+++ Config/Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINAL_PROYECTO.Config
+{
+    internal class Conexion
+    {
+        public SqlConnection conectar()
+        {
+            SqlConnection sqlConnection = null;
+            string cadenaConexion = "Data Source=161.132.40.29;Initial Catalog=ZURICH_CORP;Persist Security Info=True;User ID=estudiante;Password=Unc.2024";
+            try
+            {
+                sqlConnection = new SqlConnection(cadenaConexion);
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error en la conexion: ", ex.Message);
+            }
+            return sqlConnection;
+        }
+    }
+}
